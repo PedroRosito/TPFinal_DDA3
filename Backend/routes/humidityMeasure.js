d7efddaaed3771db922b20a1439e6f04dcd9e16c
Pedro <rositopedro@gmail.com>
2021-04-15 20:59:04 -0300

    Se añade la ruta de humidityMeasure

--- Backend/routes/humidityMeasure.js
+++ Backend/routes/humidityMeasure.js
@@ -1,13 +1,13 @@
 var express = require('express');
-var tempMeasure_router = express.Router();
+var humMeasure_router = express.Router();
 var postgreClient = require('../postgreSQL/postgreHandler');
 
 router.get('/:id', function(req, res, next) {
-  postgreClient.query("SELECT * FROM temperatureMeasure WHERE id=?",[Number(id)]).then(
+  postgreClient.query("SELECT * FROM humidityMeasure WHERE id=?",[Number(id)]).then(
     (response)=>res.send(response)
   ).catch(
     (err)=>res.send(err)
   );
 });
 
-module.exports = tempMeasure_router;
+module.exports = humMeasure_router;
