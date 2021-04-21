2b6b40460040cc44c7effeaea80442d85b525aef
Pedro <rositopedro@gmail.com>
2021-04-21 16:55:14 -0300

    Se añadio mqtt service

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IMqttMessage, IPublishOptions, MqttService } from "ngx-mqtt";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'Frontend';
+
+  private options: IPublishOptions = {qos:1};
+
+  constructor(private mqttService: MqttService){}
+
+  mqttPublish(message:string){
+    this.mqttService.publish("esp32/led/set",message,this.options);
+  }
+
 }
